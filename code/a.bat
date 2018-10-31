1e56c590b79f9296b883ed4fc1e6ca20b274f756
玄锷无梦 <wangdengkaiwy@163.com>
2018-11-01 00:09:39 +0800

    增加zip处理程序
    python调用winrar压缩文件.

---
+++ code/a.bat
@@ -0,0 +1,5 @@
+echo 'h'
+D:
+cd D:\Program Files (x86)\360
+winrar a -afzip -df -ep -ibck -m5 G:\development\myproject\stu_win32\hh.zip G:\development\myproject\stu_win32\haha\*
+echo 'ok'
---
+++ code/stu_zipfile.py
@@ -0,0 +1,19 @@
+import zipfile
+
+#
+# zf = zipfile.ZipFile('new_test',mode='w',compression=zipfile.ZIP_DEFLATED)
+# zf.write('hha/hahahaha.docx')
+# zf.write('hha/haha.docx')
+# zf.close()
+import os
+import sys
+# sys.path.append(r'D:\Program Files (x86)\360')
+# os.system('zip -r hha.zip  ./hha/*')
+# os.system(r'cd D:\Program Files (x86)\360\ ')
+# os.system(r'winrar')
+# os.system(r'cd D:\Program Files (x86)\360\ & winrar a -afzip -df -ep -ibck -m5 G:\development\myproject\stu_win32\hh.zip G:\development\myproject\stu_win32\hha')
+# os.system('dir')
+import  subprocess
+
+# subprocess.Popen('a.bat',shell=False)
+os.system('a.bat')
